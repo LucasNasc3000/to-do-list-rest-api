bda99b8937035fcbb898585f14a29f0cfc0eac32
Lucas <storagemail329@gmail.com>
2024-03-07 16:28:02 -0300

    1.0.0

---
+++ new-task.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json');
+$connection = new mysqli("localhost", "root", "", "task_list");
+
+if($connection->connect_error) {
+	echo "Error: " . $connection->connect_error;
+	exit;
+}
+$stmt = $connection->prepare("INSERT INTO task_list (tasktitle, task) VALUES (?, ?)");
+
+$task = $_POST["task"];
+$title = $_POST["title"];
+
+//ss quer dizer que os dois campos (usuário e senha) serão string
+//o método bind não recebe as strings dos campos do BD diretamente, eles precisam ser enviados por referência
+$stmt->bind_param("ss", $task, $title); 
+
+$stmt->execute();
+?>
