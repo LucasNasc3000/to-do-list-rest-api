e94fd58ecd3f448b9db5f41208122601b7474eb3
Lucas <storagemail329@gmail.com>
2024-03-15 18:52:18 -0300

    title removido e arquivo para udpate adicionado

--- new-task.php
+++ new-task.php
@@ -6,14 +6,13 @@ if($connection->connect_error) {
 	echo "Error: " . $connection->connect_error;
 	exit;
 }
-$stmt = $connection->prepare("INSERT INTO task_list (tasktitle, task) VALUES (?, ?)");
+$stmt = $connection->prepare("INSERT INTO task_list (task) VALUES (?)");
 
 $task = $_POST["task"];
-$title = $_POST["title"];
 
 //ss quer dizer que os dois campos (usuário e senha) serão string
 //o método bind não recebe as strings dos campos do BD diretamente, eles precisam ser enviados por referência
-$stmt->bind_param("ss", $task, $title); 
+$stmt->bind_param("s", $task); 
 
 $stmt->execute();
 ?>
---
+++ task-update.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-Type: application/json');
+$connection = new mysqli("localhost", "root", "", "task_list");
+
+if($connection->connect_error) {
+	echo "Error: " . $connection->connect_error;
+	exit;
+}
+$stmt = $connection->prepare("UPDATE task_list SET task=$task WHERE idtask=$urlid");
+
+$task = $_POST["task"];
+$urlid = $_POST["urlid"];
+
+$stmt->bind_param("ss", $task, $urlid); 
+
+$stmt->execute();
+?>
