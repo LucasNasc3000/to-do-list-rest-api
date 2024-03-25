1d18cc9075e50b90db191e6d09317fd292ef29d0
Lucas <storagemail329@gmail.com>
2024-03-25 18:38:51 -0300

    add comentarios

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
+// Todas as requisições deverão ser POST devido ao campo dboperation, que não é um campo na base de dados mas
+// informação de qual operação será feita com o banco de dados.
+// Assim sendo, todas as requisições do front-end deverão ter um corpo e o campo "dboperation", informando o 
+// tipo de operação (read, create, update ou search). Delete ainda não foi implementado.
+
 require_once("Task.php");
 
 $tasks = new Task();
